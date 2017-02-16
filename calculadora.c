89c235b00c9c23bcd3944788ebe702918edbb1c3
William <williambarreramuoz@gmail.com>
2017-02-16 08:28:50 -0500

    se agrego la funcion imprimir menu

---
+++ calculadora.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void imprimirMenu();
+
+int main(){
+    imprimirMenu();
+}
+
+void imprimirMenu(){
+	printf("/n CALCULADORA");
+	printf("/n 1. Suma");
+	printf("/n 2. Resta");
+	printf("/n 3. Multiplicacion");
+	printf("/n 4. Salir");
+	}
