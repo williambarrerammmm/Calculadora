f77c200dd25be796b265e96fc1f3cc38b7438add
William <williambarreramuoz@gmail.com>
2017-02-16 08:47:17 -0500

    se añade la funcion para elegir la opcion

--- calculadora.c
+++ calculadora.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
 
 void imprimirMenu();
+int leerOpcion();
 
 int main(){
     imprimirMenu();
+    
+    switch (leerOpcion()){
+		case 1:
+		      break;
+		case 2:
+		      break;
+		case 2:
+		      break;
+		case 2:
+		      break;
+	}
 }
 
 void imprimirMenu(){
@@ -12,4 +24,11 @@ void imprimirMenu(){
 	printf("/n 2. Resta");
 	printf("/n 3. Multiplicacion");
 	printf("/n 4. Salir");
-	}
+}
+
+int leerOpcion(){
+	int i_i_opcion;
+	printf("/n Digite una opcion");
+	scanf("%d%*c",&i_i_opcion);
+	return i_i_opcion;
+}
